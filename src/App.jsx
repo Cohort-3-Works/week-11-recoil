f7b49c68699ada4f7c22244c6599eeb95bb92c1b
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-04 02:32:37 +0530

    getting started with recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
+import { Counter } from "./components/Counter";
 import "./App.css";
 
 function App() {
@@ -11,46 +10,3 @@ function App() {
 }
 
 export default App;
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Count is : {count}</h1>
-      <br />
-      <Increase setCount={setCount}></Increase>
-      <Decrease setCount={setCount}></Decrease>
-    </>
-  );
-}
-
-function Increase({ setCount }) {
-  function increase() {
-    setCount((c) => c + 1);
-  }
-
-  return (
-    <>
-      <button onClick={increase}>Increase</button>
-    </>
-  );
-}
-function Decrease({ setCount }) {
-  function decrease() {
-    setCount((c) => c - 1);
-  }
-
-  return (
-    <>
-      <button onClick={decrease}>Decrease</button>
-    </>
-  );
-}
-
-Increase.propTypes = {
-  setCount: PropTypes.func.isRequired,
-};
-Decrease.propTypes = {
-  setCount: PropTypes.func.isRequired,
-};
